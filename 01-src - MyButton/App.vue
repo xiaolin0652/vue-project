9185242a6050af56587d709c8906fc190a63d067
haoxiaolin <xiaolin0652@163.com>
2020-12-14 18:22:47 +0800

    '初始化'

---
+++ 01-src - MyButton/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <MyButton></MyButton>
+  </div>
+</template>
+
+<script>
+import MyButton from '@/components/MyButton'
+export default {
+  name: '',
+  components:{
+      MyButton
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+   <div>
+    <Header></Header>
+    <div class="container">
+      <Add></Add>
+      <List></List>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Add from './components/Add'
+import List from './components/List'
+export default {
+  components: { 
+      Header ,
+      Add,
+      List
+      },
+  name: '',
+}
+</script>
+
+<style scoped>
+
+</style>
